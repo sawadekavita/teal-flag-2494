031bc740e1d8517b341baaab4cc071b828f5be0f
kavita kharat <sawadekavita.1102@gmail.com>
2022-09-29 00:23:14 +0530

    day2 added

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -6,7 +6,7 @@ const Signin = () => {
 
         <h1>More to Rack, easier and faster.</h1>
         {/* <input>Signin or create Account</input> */}
-        <input type="text" placeholder="Signin or create Account" />
+        <button disabled="disabled">Signin or create Account</button>
     </div>
   )
 }
